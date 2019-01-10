6b85a2908c969a594723864b51a9fbd8bbee219c
Joao Silva <silva.joao@live.com.pt>
2019-01-10 16:57:48 -0100

    Fixing Admin template, adding Gulp compiled

--- gulpfile.js
+++ gulpfile.js
@@ -23,8 +23,8 @@ elixir(function(mix) {
         'libs/styles.css'
     ], './public/css/libs.css')
     .scripts([
-        'libs/bootstrap.js',
         'libs/jquery.js',
+        'libs/bootstrap.js',
         'libs/metisMenu.js',
         'libs/sb-admin-2.js',
         'libs/scripts.js'
