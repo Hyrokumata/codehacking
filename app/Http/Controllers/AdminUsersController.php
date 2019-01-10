f2aac89643806cdc1dc43b15cf4a55b8f90e46ec
Joao Silva <silva.joao@live.com.pt>
2019-01-10 16:39:11 -0100

    Install Admin template, configuring Gulp for Laravel 5.2

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -15,7 +15,7 @@ class AdminUsersController extends Controller
      */
     public function index()
     {
-        //
+        return view('admin.users.index');
     }
 
     /**
@@ -25,7 +25,7 @@ class AdminUsersController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.users.create');
     }
 
     /**
@@ -36,7 +36,7 @@ class AdminUsersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        return view('admin.users.store');
     }
 
     /**
@@ -47,7 +47,7 @@ class AdminUsersController extends Controller
      */
     public function show($id)
     {
-        //
+        return view('admin.users.show');
     }
 
     /**
@@ -58,7 +58,7 @@ class AdminUsersController extends Controller
      */
     public function edit($id)
     {
-        //
+        return view('admin.users.edit');
     }
 
     /**
@@ -70,7 +70,7 @@ class AdminUsersController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        return view('admin.users.update');
     }
 
     /**
@@ -81,6 +81,6 @@ class AdminUsersController extends Controller
      */
     public function destroy($id)
     {
-        //
+        return view('admin.users.destroy');
     }
 }
