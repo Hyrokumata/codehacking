52812fa43fbcc7c911b3667ad495aeb2082ba59e
Joao Silva <silva.joao@live.com.pt>
2019-01-14 14:59:46 -0100

    Installing LaravelCollectiveHtml

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -4,4 +4,16 @@
 
     <h1>Admin Users Create</h1>
 
+    {!! Form::open(['method' => 'POST', 'action' => 'AdminUsersController@store']) !!}
+
+        <div class="form-group">
+            {!! Form::label('title', 'Title:') !!}
+            {!! Form::text('title', null, ['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
+        </div>
+
+    {!! Form::close() !!}
 @endsection
